2d4ae19f39ee27b9481fde91ef2f37d8d88cbdfd
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2021-04-28 03:25:44 +0700

    Added color.js

---
+++ src/color.js
@@ -0,0 +1,73 @@
+export const colorPrimary = '#00ab6b';
+export const colorPrimaryDark = '#006d32';
+export const colorDanger = '#ee4a49';
+export const colorWhite = '#fff';
+export const colorGrey = '#aaa';
+export const colorBackgroundLight = '#f9f9f9';
+export const colorBackgroundDark = '#121212';
+export const colorBoxShadow = '#ddd';
+export const colorBoxShadowDark = '#ddd';
+export const colorBorderLight = '#ededed';
+export const colorBorderDark = '#30363d';
+export const colorBorder = '#ededed';
+export const colorTextGrey = '#949494';
+export const colorTextGreySecondaryLight = '#333333';
+export const colorTextGreySecondaryDark = '#C9D1D9';
+export const colorNotFound = '#f98f06';
+
+export const global = {
+    transitionFast: '0.3s',
+    colorWhite: '#fff',
+    colorNotFound: '#f98f06',
+}
+
+export const light = {
+    colorPrimary: '#00ab6b',
+    colorButtonPrimary: '#00ab6b',
+    colorButtonOutlinePrimary: '#00ab6b',
+    colorButtonOutlinePrimaryHover: '#00ab6b',
+    colorButtonDanger: 'transparent',
+    colorButtonDangerHover: '#ee4a49',
+    colorButtonDisabled: '#ccc',
+    colorButtonDisabledHover: '#ccc',
+    colorBorderButtonDanger: '#ee4a49',
+    colorBorderButtonDangerHover: '#ee4a49',
+    colorBgSwalErrorValidation: '#f0f0f0',
+    colorBgPrimary: '#f9f9f9',
+    colorBgCardPrimary: '#fff', // Parent Card
+    colorBgCardSecondary: '#fff', // Child Card
+    colorBorderCard: '#ededed', // Border Parent Card
+    colorTextDanger: '#ee4a49',
+    colorTextDangerHover: '#fff',
+    colorTextPrimary: '#595959', //#040d21
+    colorTextSecondary: '#333',
+    colorTextTertiary: '#949494',
+    colorBgChips: '#aaa',
+    colorTextChips: '#fff',
+    colorBoxShadow: '#ddd'
+}
+export const dark = {
+    colorPrimary: '#00ab6b',
+    colorButtonPrimary: '#006d32',
+    colorButtonOutlinePrimary: '#00ab6b',
+    colorButtonOutlinePrimaryHover: '#006d32',
+    colorButtonDanger: '#21262d',
+    colorButtonDangerHover: '#21262d',
+    colorButtonDisabled: '#222',
+    colorButtonDisabledHover: '#222',
+    colorBorderButtonDanger: '#30363d',
+    colorBorderButtonDangerHover: '#8b949e',
+    colorBgSwalErrorValidation: '#30363d',
+    colorBgPrimary: '#121212',
+    colorBgCardPrimary: '#121212', // Parent Card
+    colorBgCardSecondary: '#161b22', // Child Card
+    colorBorderCard: '#30363d', // Border Parent Card
+    colorTextDanger: '#fff',
+    colorTextDangerHover: '#fff',
+    colorTextPrimary: '#C9D1D9',
+    colorTextSecondary: '#C9D1D9',
+    colorTextTertiary: '#949494',
+    colorBgChips: '#21262d',
+    colorTextChips: '#c9d1d9',
+    colorBoxShadow: ''
+}
